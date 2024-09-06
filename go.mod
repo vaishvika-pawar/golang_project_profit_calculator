66d05dfb57322944a6c9842afafbb50ace8def83
vaishvika <vaishvika.pawar@kotak.com>
2024-09-06 14:45:37 +0530

    Profit calculator with user input and showing output in formatted way.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module magicosmica.com/golang_profit_calculator
+
+go 1.23.1
---
+++ profit_calculator.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+
+	fmt.Print("Revenue: ")
+	fmt.Scan(&revenue)
+	fmt.Print("Expenses: ")
+	fmt.Scan(&expenses)
+	fmt.Print("Tax Rate: ")
+	fmt.Scan(&taxRate)
+
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+
+	println("Earning Before Tax: ", ebt)
+	println("Profit: ", profit)
+	println("Ratio: ", ratio)
+}
