df489b60e6b6dd13a80f9d924f03c1847ee3cab4
vaishvika <vaishvika.pawar@kotak.com>
2024-09-06 17:22:28 +0530

    funtions, return values, variable scope and alternative return value syntax

--- profit_calculator.go
+++ profit_calculator.go
@@ -3,20 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+	revenue := getUserInput("Revenue: ")
+	expenses := getUserInput("Expenses: ")
+	taxRate := getUserInput("Tax Rate: ")
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
+	fmt.Printf("Earning Before Tax: %.2f\nProfit: %.2f\nRatio: %.2f", ebt, profit, ratio)
+}
+
+func getUserInput(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
 	ratio := ebt / profit
-
-	fmt.Printf("Earning Before Tax: %.2f\nProfit: %.2f\nRatio: %.2f", ebt, profit, ratio)
+	return ebt, profit, ratio
 }
